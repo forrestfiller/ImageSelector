76c894f13094051dd9b51da8dbf27e27fe85a7cc
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-11 19:42:20 -0400

    added right bar nav button named 'pick image'

--- ImageSelector/ISViewController.swift
+++ ImageSelector/ISViewController.swift
@@ -27,36 +27,36 @@ class ISViewController: UIViewController, UITableViewDataSource, UITableViewDele
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Pick Image",
+            style:  .Plain,
+            target:  self,
+            action:  nil
+        )
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellId = "cellId"
         if let cell = tableView.dequeueReusableCellWithIdentifier(cellId){
             cell.textLabel?.text = "\(indexPath.row)"
             return cell
         }
+        
         let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
         cell.textLabel?.text = "\(indexPath.row)"
         return cell
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
